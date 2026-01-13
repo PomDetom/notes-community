59bb9637cc040ff0f722b3cdefaa6f10ae262ec4
PomDetom <pomdetom@gmail.com>
2026-01-13 20:10:31 +0800

    version-0.1.0 引入部分model,测试数据库连接,从nacos读取配置

---
+++ notes-test-provider/src/main/java/com/pomdetom/notes/mapper/TestServiceMapper.java
@@ -0,0 +1,12 @@
+package com.pomdetom.notes.mapper;
+
+import com.pomdetom.notes.test.model.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface TestServiceMapper {
+
+    @Select("SELECT * FROM user WHERE user_id = 100000")
+    User getUser();
+}
--- notes-test-provider/src/main/java/com/pomdetom/notes/provider/TestServiceImpl.java
+++ notes-test-provider/src/main/java/com/pomdetom/notes/provider/TestServiceImpl.java
@@ -1,13 +1,22 @@
 package com.pomdetom.notes.provider;
 
+import com.pomdetom.notes.mapper.TestServiceMapper;
+import com.pomdetom.notes.test.model.entity.User;
 import com.pomdetom.notes.test.service.TestService;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 @DubboService
 public class TestServiceImpl implements TestService {
+
+    @Resource
+    private TestServiceMapper testServiceMapper;
+
     @Override
     public String sayHello(String name) {
-        return "Hello " + name;
+        User user = testServiceMapper.getUser();
+        return "Hello " + name + " " + user;
     }
 }
